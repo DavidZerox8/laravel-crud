fd7c85a3693ff6d1b55fda39503f946701c59c47
DavidZerox8 <darkslayer_zerox8@hotmail.com>
2021-11-21 12:07:51 -0600

    se añadio el navbar

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,65 +1,129 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <title>Lista de usuarios - CRUD</title>
 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <title>Lista de usuarios - CRUD</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body>
-        <div class="container">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!--Navbar -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="logo-details">
+            <div class="logo_name">David ML</div>
+            <i class='bx bx-menu' id="btn"></i>
+        </div>
+        <ul class="nav-list">
+            <li>
+                <i class='bx bx-search'></i>
+                <input type="text" placeholder="Buscar...">
+                <span class="tooltip">Buscar</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-grid-alt'></i>
+                    <span class="links_name">Sobre mí</span>
+                </a>
+                <span class="tooltip">Sobre mí</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-user'></i>
+                    <span class="links_name">Cursos</span>
+                </a>
+                <span class="tooltip">Cursos</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-chat'></i>
+                    <span class="links_name">Experiencia laboral</span>
+                </a>
+                <span class="tooltip">Experiencia laboral</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-pie-chart-alt-2'></i>
+                    <span class="links_name">Educación</span>
+                </a>
+                <span class="tooltip">Educación</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-folder'></i>
+                    <span class="links_name">File Contacto</span>
+                </a>
+                <span class="tooltip">Contacto</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-cart-alt'></i>
+                    <span class="links_name">Galeria</span>
+                </a>
+                <span class="tooltip">Galeria</span>
+            </li>
+        </ul>
+    </div>
+    <section class="home-section">
+        <div class="container text">
             <div class="row">
                 <div class="col-sm-8 mx-auto">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        ID
-                                    </th>
-                                    <th>
-                                        Nombre
-                                    </th>
-                                    <th>
-                                        Correo
-                                    </th>
-                                    <th>
-                                        &nbsp;
-                                    </th>
-                                </tr>
-                            </thead> 
-                            <tbody>
-                                @foreach ($users as $user)
-                                    <tr>
-                                        <td>
-                                            {{ $user->id }}                                        
-                                        </td>
-                                        <td>                                        
-                                            {{ $user->name }}                                        
-                                        </td>
-                                        <td>                                        
-                                            {{ $user->email }}
-                                        </td>
-                                        <td>
-                                            <form action="{{ route('user.destroy',$user) }}" method="POST">
-                                                @method('DELETE')
-                                                @csrf
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>
+                                    ID
+                                </th>
+                                <th>
+                                    Nombre
+                                </th>
+                                <th>
+                                    Correo
+                                </th>
+                                <th>
+                                    &nbsp;
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                            <tr>
+                                <td>
+                                    {{ $user->id }}
+                                </td>
+                                <td>
+                                    {{ $user->name }}
+                                </td>
+                                <td>
+                                    {{ $user->email }}
+                                </td>
+                                <td>
+                                    <form action="{{ route('user.destroy',$user) }}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
 
-                                                <input type="submit" value="Eliminar" class="btn btn-danger" onclick="return confirm('¿Desea eliminar el registro?')">
+                                        <input type="submit" value="Eliminar" class="btn btn-danger" onclick="return confirm('¿Desea eliminar el registro?')">
 
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach   
-                            </tbody>                               
-                        </table>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
-            </div>                
-        </div>        
-    </body>
+            </div>
+        </div>
+    </section>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+</body>
+
 </html>
